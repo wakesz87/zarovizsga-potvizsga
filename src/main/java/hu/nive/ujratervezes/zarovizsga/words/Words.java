f49907b7c6045af07819027d5b21f0bf9716cb3c
wakesz87 <kisszab87@gmail.com>
2021-03-22 22:11:24 +0100

    First_Commit / Start

--- src/main/java/hu/nive/ujratervezes/zarovizsga/words/Words.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/words/Words.java
@@ -2,4 +2,16 @@ package hu.nive.ujratervezes.zarovizsga.words;
 
 public class Words {
 
+    public boolean hasMoreDigits(String s) {
+        if (s == null || s.isBlank()) {
+            return false;
+        }
+        int digits = 0;
+        for (char c : s.toCharArray()) {
+            if (Character.isDigit(c)) {
+                digits++;
+            }
+        }
+        return digits > (s.length() - digits);
+    }
 }
